bc62209335d09f868385c313650885b96a49e0f3
Nicole Carpenter <nicolecarpenterusa@gmail.com>
2016-01-21 21:19:55 -0600

    final refactored solution for single responsibility

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ require_relative 'player'
 require_relative 'view'
 
 class Game
-  attr_accessor :players, :type
+  attr_accessor :players
   attr_reader :board
 
   def initialize
@@ -11,81 +11,42 @@ class Game
     @view = View.new
     @board = Board.new(9)
     @active_board = @board.active_board
-    @type = nil
   end
 
   def setup_game
     @view.clear_screen!
-    @view.get_player_name(@players[0])
-    get_board_pieces
-    game_type_selection
-    unless @type == "3"
-      @view.display_order_options(@players[0])
-      player_order_selection
-      @view.confirm_player_order(@players[0])
+    puts @view.display_name_prompt
+    @view.assign_player_name(@players[0])
+    puts
+
+    puts @view.display_pieces_prompt
+    marks = @view.confirm_valid_pieces
+    @view.assign_pieces_to_player(@players, marks)
+    puts
+
+    puts @view.display_game_options
+    selection = @view.get_user_input
+    type = @view.confirm_valid_type(selection)
+    puts
+    sleep(1.0)
+    puts @view.display_game_confirmation(type)
+    puts
+
+    @view.assign_opponent(type, @players)
+    @view.set_demo_players(type, @players)
+    puts
+
+    unless type == "3"
+      puts @view.display_order_options(@players[0])
+      selection = @view.get_user_input
+      order = @view.confirm_valid_order(selection)
+      if order == "2"
+        @players.reverse!
+      end
+      puts
+      puts @view.display_order_confirmation(@players[0])
     end
     sleep(2.0)
-  end
-
-  def two_unique_pieces(marks)
-    marks.length == 2 && marks.uniq.length > 1
-  end
-
-  def assign_pieces(players, marks)
-    players[0].mark = marks[0]
-    players[1].mark = marks[1]
-  end
-
-  def check_for_valid_pieces(marks, players)
-    if two_unique_pieces(marks)
-      assign_pieces(players, marks)
-    else
-      @view.display_invalid_char_message
-    end
-  end
-
-  def get_board_pieces
-    @view.display_pieces_prompt
-    marks = []
-    until two_unique_pieces(marks)
-      marks = @view.get_pieces
-      check_for_valid_pieces(marks, players)
-    end
-    puts
-  end
-
-  def game_type_selection
-    @view.display_game_options
-    @type = @view.get_user_input
-    if @type == "1"
-      @view.confirm_player_vs_comp
-    elsif @type == "2"
-      @view.confirm_player_vs_player(@players)
-    elsif @type == "3"
-      @view.confirm_comp_vs_comp(@players)
-    else
-      @view.display_invalid_selection_message
-      game_type_selection
-    end
-    puts
-  end
-
-  def reverse_player_order
-    @players[0].order = 2
-    @players = @players.reverse
-  end
-
-  def player_order_selection
-    order = @view.get_user_input.to_i
-    until order == 1 || order == 2
-      @view.display_invalid_selection_message
-      @view.display_order_options(@players[0])
-      order = @view.get_user_input.to_i
-    end
-    if order == 2
-      reverse_player_order
-    end
-    puts
   end
 
   def play_game
